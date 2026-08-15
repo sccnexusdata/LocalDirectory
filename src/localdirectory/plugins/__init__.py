c0548845fe0124e0a0e23b3c6e46f5871b9b4500
sccnexusdata <sccnexusdata@gmail.com>
2026-08-15 16:41:32 +0100

    Export Lewes Chamber plugin

--- src/localdirectory/plugins/__init__.py
+++ src/localdirectory/plugins/__init__.py
@@ -1,7 +1,15 @@
 from localdirectory.plugins.companies_house import CompaniesHousePlugin
 from localdirectory.plugins.fhrs import FHRSPlugin
 from localdirectory.plugins.json_ld import JSONLDPlugin
+from localdirectory.plugins.lewes_chamber import LewesChamberPlugin
 from localdirectory.plugins.manual_csv import ManualCSVPlugin
 from localdirectory.plugins.osm_overpass import OSMOverpassPlugin
 
-__all__ = ["CompaniesHousePlugin", "FHRSPlugin", "JSONLDPlugin", "ManualCSVPlugin", "OSMOverpassPlugin"]
+__all__ = [
+    "CompaniesHousePlugin",
+    "FHRSPlugin",
+    "JSONLDPlugin",
+    "LewesChamberPlugin",
+    "ManualCSVPlugin",
+    "OSMOverpassPlugin",
+]
